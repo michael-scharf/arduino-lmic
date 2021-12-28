db63df3dff6a6ef3cdb5a882b1c645c08d21b781
Michael Scharf <michael.scharf@gmail.com>
2021-12-28 19:21:50 +0100

    Patch suggested by Matthias Machtolf

--- project_config/lmic_project_config.h
+++ project_config/lmic_project_config.h
@@ -1,6 +1,6 @@
 // project-specific definitions
-//#define CFG_eu868 1
-#define CFG_us915 1
+#define CFG_eu868 1
+//#define CFG_us915 1
 //#define CFG_au915 1
 //#define CFG_as923 1
 // #define LMIC_COUNTRY_CODE LMIC_COUNTRY_CODE_JP      /* for as923-JP; also define CFG_as923 */
@@ -8,3 +8,5 @@
 //#define CFG_in866 1
 #define CFG_sx1276_radio 1
 //#define LMIC_USE_INTERRUPTS
+
+#define hal_init LMICHAL_init
